82f8fa61de45e9a5085748399cf89fde694c23ae
AdelHamed-X <adelhamed349.ah@gmail.com>
2023-11-28 11:05:30 +0200

    Adding Task 0

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -24,6 +24,8 @@ binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 		new->parent = parent;
 		if (parent->left != NULL)
 			parent->right = new;
+		else
+			parent->left = new;
 	}
 
 	return (new);
