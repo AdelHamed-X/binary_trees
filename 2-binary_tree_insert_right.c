b47f6e689a1c902ddfb3e4cb80a2570bd6aa1d08
AdelHamed-X <adelhamed349.ah@gmail.com>
2023-11-28 13:02:59 +0200

    Adding Task 2

--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -17,7 +17,7 @@ binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 
 	new = binary_tree_node(parent, value);
 
-	if (parent->left != NULL)
+	if (parent->right != NULL)
 	{
 		new->right = parent->right;
 		parent->right->parent = new;
