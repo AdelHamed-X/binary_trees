60cdeb62eea3c3b24271e02d62a7b2529356cf2f
AdelHamed-X <adelhamed349.ah@gmail.com>
2023-11-29 12:03:47 +0200

    Adding Task 9

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -13,12 +13,17 @@ size_t binary_tree_height(const binary_tree_t *tree)
         
         if (tree == NULL)
                 return (0);
-
-        right_depth = binary_tree_height(tree->right);
-        left_depth = binary_tree_height(tree->left);
+        if (tree->right != NULL)
+                right_depth = 1 + binary_tree_height(tree->right);
+        else
+                right_depth = 0;
+        if (tree->left != NULL)
+                left_depth = 1 + binary_tree_height(tree->left);
+        else
+                left_depth = 0;
 
         if (right_depth > left_depth)
-                return (right_depth + 1);
+                return (right_depth);
         else
-                return (left_depth + 1);
+                return (left_depth);
 }
