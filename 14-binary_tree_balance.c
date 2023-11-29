23a94ae6473194fcd97277af10916d70313c5437
AdelHamed-X <adelhamed349.ah@gmail.com>
2023-11-29 14:29:49 +0200

    Adding Task 14

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -9,13 +9,25 @@
 */
 int binary_tree_balance(const binary_tree_t *tree)
 {
-        size_t right_height = 0, left_height = 0;
-        
+        return (binary_tree_height(tree->left) - binary_tree_height(tree->right));
+}
+
+/**
+ * binary_tree_height - measures the height of a binary tree
+ * 
+ * @tree: a pointer to the root node
+ * 
+ * Return: the heihgt of the tree
+*/
+size_t binary_tree_height(const binary_tree_t *tree)
+{
+        size_t right_height, left_height;
+
         if (tree == NULL)
                 return (0);
-        
-        right_height = (tree->right) ? (1 + binary_tree_balance(tree->right)) : 0; 
-        left_height = (tree->left) ? (1 + binary_tree_balance(tree->left)) : 0; 
 
-        return (left_height - right_height);
+        right_height = (tree->right) ? (1 + binary_tree_height(tree->right)) : 1; 
+        left_height = (tree->left) ? (1 + binary_tree_height(tree->left)) : 1; 
+
+        return ((right_height > left_height) ? right_height : left_height);
 }
