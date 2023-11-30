7159f31d26ac3782bd9af9c449ec0d5e7eaf8c4e
AdelHamed-X <adelhamed349.ah@gmail.com>
2023-11-30 14:00:24 +0200

    Adding Task 4

--- 4-binary_tree_is_leaf.c
+++ 4-binary_tree_is_leaf.c
@@ -2,18 +2,18 @@
 
 /**
  * binary_tree_is_leaf - checks if a node is leaf
- * 
+ *
  * @node: the node to check
- * 
+ *
  * Return: 1 if a node is leaf, 0 otherwise
-*/
+ */
 int binary_tree_is_leaf(const binary_tree_t *node)
 {
-        if (node != NULL)
-        {
-                if (node->left == NULL && node->right == NULL)
-                        return (1);
-        }
+	if (node != NULL)
+	{
+		if (node->left == NULL && node->right == NULL)
+			return (1);
+	}
 
-        return (0);
+	return (0);
 }
