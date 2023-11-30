ef978cb6c37d4465894a07bfa9fa98645bfd3636
AdelHamed-X <adelhamed349.ah@gmail.com>
2023-11-30 14:06:50 +0200

    Adding Task 8

--- 7-binary_tree_inorder.c
+++ 7-binary_tree_inorder.c
@@ -2,16 +2,16 @@
 
 /**
  * binary_tree_inorder - goes through a binary tree using in-order traversal
- * 
+ *
  * @tree: a pointer to the root node
  * @func: a function to call for each node
-*/
+ */
 void binary_tree_inorder(const binary_tree_t *tree, void (*func)(int))
 {
-        if (tree == NULL || func == NULL)
-                return;
+	if (tree == NULL || func == NULL)
+		return;
 
-        binary_tree_inorder(tree->left, func);
-        func(tree->n);
-        binary_tree_inorder(tree->right, func);
+	binary_tree_inorder(tree->left, func);
+	func(tree->n);
+	binary_tree_inorder(tree->right, func);
 }
