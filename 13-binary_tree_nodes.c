b6e24efbb49b23951b0671d4137e5b865e8d0224
AdelHamed-X <adelhamed349.ah@gmail.com>
2023-11-30 14:25:04 +0200

    Adding Task 13

--- 13-binary_tree_nodes.c
+++ 13-binary_tree_nodes.c
@@ -2,24 +2,24 @@
 
 /**
  * binary_tree_nodes - counts the nodes with at least 1 child in a binary tree.
- * 
+ *
  * @tree: a pointer to the root node
- * 
+ *
  * Return: the count of the tree nodes with at least 1 child
-*/
+ */
 size_t binary_tree_nodes(const binary_tree_t *tree)
 {
-        size_t size = 0;
-        
-        if (tree)
-        {
-                if (tree->right || tree->left)
-                {
-                        size += 1; 
-                } 
-                size += binary_tree_nodes(tree->right); 
-                size += binary_tree_nodes(tree->left);
-        }
+	size_t size = 0;
 
-        return (size);
+	if (tree)
+	{
+		if (tree->right || tree->left)
+		{
+			size += 1;
+		}
+		size += binary_tree_nodes(tree->right);
+		size += binary_tree_nodes(tree->left);
+	}
+
+	return (size);
 }
