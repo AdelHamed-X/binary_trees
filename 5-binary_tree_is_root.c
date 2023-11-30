f44eb25308646fae4d5a51646ea67c1fe351117d
AdelHamed-X <adelhamed349.ah@gmail.com>
2023-11-30 14:02:08 +0200

    Adding Task 5

--- 5-binary_tree_is_root.c
+++ 5-binary_tree_is_root.c
@@ -2,18 +2,18 @@
 
 /**
  * binary_tree_is_root - check is a node is root
- * 
+ *
  * @node: the node to check
- * 
+ *
  * Return: 1 if the node is root, 0 otherwise
-*/
+ */
 int binary_tree_is_root(const binary_tree_t *node)
 {
-        if (node != NULL)
-        {
-                if (node->parent == NULL)
-                        return (1);
-        }
+	if (node != NULL)
+	{
+		if (node->parent == NULL)
+			return (1);
+	}
 
-        return (0);
+	return (0);
 }
