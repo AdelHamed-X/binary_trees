971e6c724402632b0364f24ee6cd41ed3247afef
AdelHamed-X <adelhamed349.ah@gmail.com>
2023-11-30 12:56:45 +0200

    Adding Task 15

--- 15-binary_tree_is_full.c
+++ 15-binary_tree_is_full.c
@@ -9,17 +9,11 @@
 */
 int binary_tree_is_full(const binary_tree_t *tree)
 {
-        int check;
+        if (tree == NULL)
+                return (0);
 
-        if (tree)
-        {
-                if (tree->left && tree->right)
-                {
-                        check = 1;
-                }
-                binary_tree_is_full(tree->left);
-                binary_tree_is_full(tree->right);
-        }
+        if (tree->left== NULL && tree->right == NULL)
+                return (1);
 
-        return (check);
+        return (tree->left) ? binary_tree_is_full(tree->left) : binary_tree_is_full(tree->left);
 }
