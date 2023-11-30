90ce64eb7fefe6e9867eeafa2e961313c194a248
AdelHamed-X <adelhamed349.ah@gmail.com>
2023-11-30 15:59:17 +0200

    Adding Task 18

---
+++ 18-binary_tree_uncle.c
@@ -0,0 +1,31 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_uncle - finds the uncle of a node.
+ *
+ * @node: a pointer ot the root node.
+ *
+ * Return: a pointer to the uncle node, (NULL) otherwise
+ */
+binary_tree_t *binary_tree_uncle(binary_tree_t *node)
+{
+	if (node && node->parent->parent)
+	{
+		return (check_node_position(node->parent));
+	}
+	return (NULL);
+}
+
+/**
+ * check_node_position - checks if the node is on right or left
+ *
+ * @tree: a node
+ *
+ * Return: a pointer to the sibling node or NULL
+ */
+binary_tree_t *check_node_position(binary_tree_t *tree)
+{
+	if (tree->parent->right->n == tree->n)
+		return (tree->parent->left);
+	return (tree->parent->right);
+}
